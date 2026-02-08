00a9b8e15926e838e00beafec500b0c30ced8069
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-08 15:59:13 +0530

    add if else if else statement

---
+++ if_else_if.dart
@@ -0,0 +1,10 @@
+void main() {
+  int number = 5;
+  if (number > 20) {
+    print("Number is greater than 20");
+  } else if (number > 10) {
+    print("Number is greater than 10 but less than or equal to 20");
+  } else if (number > 5) {
+    print("Number is greater than 5 but less than or equal to 10");
+  }
+}
